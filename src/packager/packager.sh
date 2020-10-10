ed9b3b6dad816c360925ec1eb9c19a2f095dc557
GHIFARI160 <ghifari@ghifari160.com>
2020-10-10 14:22:17 -0500

    Implement packager Linux support
    The packaging tool will now work properly in Linux, provided that rsync is installed.
    
    - Implement packager Linux support

--- src/packager/packager.sh
+++ src/packager/packager.sh
@@ -14,7 +14,9 @@ echo "Packaging..."
 mkdir -p ${1}
 
 if [ $machine == "Linux" ]; then
-    echo "To Do"
+    zip -r -X "${1}/${2}-v${3}.zip" . -x "*.git*"
+    tar --exclude="*.git*" -zcvf "${1}/${2}-v${3}.tar.gz" .
+    tar --exclude="*.git*" -jcvf "${1}/${2}-v${3}.tar.bz2" .
 elif [ $machine == "Mac" ]; then
     zip -r -X "${1}/${2}-v${3}.zip" . -x "*.git*" "*.DS_Store"
     tar --exclude="*.git*" --exclude="*.DS_Store" -zcvf "${1}/${2}-v${3}.tar.gz" .
